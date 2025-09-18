53258b9dc129e753682db0f7c77737d3aa455a6c
NF <nasrllah15@gmail.com>
2025-09-18 17:06:37 +0100

    Added the necessary NuGet Packages

--- obj/Debug/net9.0/FlashCardsAppBE.AssemblyInfo.cs
+++ obj/Debug/net9.0/FlashCardsAppBE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlashCardsAppBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c81347a564f1ba17f7419b679389dde7b83a10d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlashCardsAppBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlashCardsAppBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
